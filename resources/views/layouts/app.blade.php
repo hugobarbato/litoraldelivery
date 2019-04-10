a198a31ff64a2293822237cb9c95e53784530805
Hugo Barbato <hugobarbato@gmail.com>
2019-04-10 18:52:22 +0000

    Savando o repositorio do c9

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <title>Laravel Quickstart - Basic</title>
+
+        <!-- CSS And JavaScript -->
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar Contents -->
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
